1f425a078c742ea5e8507c7438daec5d21842903
paranoid <paranoid@paranoidQ>
2014-08-25 22:28:23 +0800

    codes

--- Codes/src/snap_amazon_meta/Importer.java
+++ Codes/src/snap_amazon_meta/Importer.java
@@ -20,7 +20,6 @@ public class Importer {
 	private static BufferedReader reader;
 	private static Connection connection;
 	
-	
 	// store mata 
 	private static int id = 0;
 	private static String asin = null;
