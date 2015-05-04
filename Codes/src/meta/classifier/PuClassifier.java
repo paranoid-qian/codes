9bfe9a9fff3744bb0ddc7607f2a3731c972d17d0
paranoid.Q <paranoid.Q@paranoidQ>
2015-05-04 09:53:31 +0800

    remove redundency imports

--- Codes/src/meta/classifier/PuClassifier.java
+++ Codes/src/meta/classifier/PuClassifier.java
@@ -1,9 +1,6 @@
 package meta.classifier;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
